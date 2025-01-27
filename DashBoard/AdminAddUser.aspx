b2b2085cb51d675e5de46407951fbebef6f68424
mina-atef-tech <83976389+mina-atef-tech@users.noreply.github.com>
2025-01-27 15:27:06 +0200

    Add files via upload

---
+++ DashBoard/AdminAddUser.aspx
@@ -0,0 +1,105 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/DashBoard/Dashboard.Master" AutoEventWireup="true" CodeBehind="AdminAddUser.aspx.cs" Inherits="Blood_For_Life.DashBoard.AdminAddUser" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+        <div class="container" dir="rtl">
+              <div class="row">
+    <div class="col-md-6 mx-auto">
+        <div class="card mt-4" >   
+            <div class="card-body m-3">  
+
+                <%--UserType--%>
+                <div class="row">
+                     <label for="AdminAddUserDropDownList" dir="rtl" class="d-flex align-items-start labelFont">المستخدم</label>
+
+                     <asp:DropDownList ID="AdminAddUserDropDownList" CssClass="form-control" runat="server" ></asp:DropDownList>
+              </div>
+
+               <%--Name--%>
+                <div class="row">
+           <label for="UserName" dir="rtl" class="d-flex align-items-start labelFont">الاسم</label>
+           <asp:TextBox placeholder="الاسم" id="UserName" runat="server" CssClass="form-control" TextMode="SingleLine" ></asp:TextBox>
+
+                </div>
+
+                <%--SSN--%>
+                <div class="row">
+                    
+                <label for="UserSSN" dir="rtl" class="d-flex align-items-start labelFont">الرقم القومى</label>
+                <asp:TextBox placeholder="الرقم القومى" id="UserSSN" runat="server" CssClass="form-control" TextMode="SingleLine" ></asp:TextBox>
+                </div>
+
+                <%--Mobile--%>
+                <div class="row">
+    <label for="UserMob" dir="rtl" class="d-flex align-items-start labelFont">موبايل</label>
+    <asp:TextBox placeholder="موبايل" id="UserMob" runat="server" CssClass="form-control" TextMode="SingleLine" ></asp:TextBox>
+                </div>
+
+                <%--Password--%>
+                <div class="row">
+             <label for="UserPassword" dir="rtl" class="d-flex align-items-start labelFont">كلمة السر</label>
+             <asp:TextBox placeholder="كلمة السر" id="UserPassword" runat="server" CssClass="form-control" TextMode="SingleLine" ></asp:TextBox>
+
+                    
+                </div>
+
+                <div class="row">
+                    <br />
+                </div>
+
+                <%--Search--%>
+                <div class="row">
+                    <div class="col-md-6">
+                   <asp:Button class="btn btn-info btn-block" ID="SearchBtn" onclick="SearchBtn_Click" runat="server" Text="بحث"   />
+                    </div>
+
+                    <div class="col-md-6">
+                        <asp:TextBox placeholder="ادخل الرقم القومى" CssClass="form-control" ID="SearchTxt" runat="server" TextMode="SingleLine"></asp:TextBox>
+                    </div>
+
+
+                </div>
+
+                <%--Button--%>
+                <div class="row mt-3">
+
+                    <%--add--%>
+                <div class="col-md-4">
+               <asp:Button class="btn btn-success btn-block" ID="AddUserBtn" runat="server" onclick="AddUserBtn_Click" Text="اضافة"  />
+
+                    <div class="row">
+                            <asp:TextBox placeholder="test" id="test" runat="server" CssClass="form-control" TextMode="SingleLine" ></asp:TextBox>
+
+                    </div>
+
+
+                </div>
+
+
+                    <%--Edit--%>
+                 <div class="col-md-4">
+                    <asp:Button class="btn btn-warning btn-block" ID="EdirBtn" OnClick="EdirBtn_Click" runat="server" Text="تعديل"  />
+      
+                    </div>
+
+                    <%--Delete--%>
+                 <div class="col-md-4">
+                <asp:Button class="btn btn-danger btn-block" ID="DeleteBtn" OnClick="DeleteBtn_Click" runat="server" Text="حذف"   />
+                 </div>
+                 </div>
+
+
+
+                </div>
+
+
+                
+        </div>
+
+</div>
+
+
+  </div>
+
+ </div>
+</asp:Content>
---
+++ DashBoard/AdminAddUser.aspx.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Blood_For_Life.DashBoard
+{
+    public partial class AdminAddUser : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-RNCRESV;Initial Catalog=BloodForLife;Integrated Security=True;Encrypt=False");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            GetUserType();
+        }
+
+        void GetUserType()
+        {
+            if (con.State == ConnectionState.Closed)
+                con.Open();
+
+            SqlCommand cmd = new SqlCommand("select * from UserTypeTB", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            AdminAddUserDropDownList.DataSource = dt;
+            AdminAddUserDropDownList.DataTextField = "UserType";
+            AdminAddUserDropDownList.DataValueField = "UserID";
+            AdminAddUserDropDownList.DataBind();
+        }
+        void GetUser()
+        {
+
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+            SqlCommand cmd = new SqlCommand("SELECT * from UsersTB where UserSSN = '" + SearchTxt.Text.Trim() + "' ", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            if (dt.Rows.Count >= 1)
+            {
+                UserName.Text = dt.Rows[0][1].ToString();
+                UserSSN.Text = dt.Rows[0][2].ToString();
+                UserMob.Text = dt.Rows[0][3].ToString();
+                UserPassword.Text = dt.Rows[0][4].ToString();
+
+            }
+            else
+            {
+                Response.Write("<script>alert('الرقم القومى خطأ');</script>");
+
+            }
+        }
+
+        void ClearForm()
+        {
+            UserName.Text = "";
+            UserSSN.Text = "";
+            UserMob.Text = "";
+            UserPassword.Text = "";
+        }
+
+
+        protected void AddUserBtn_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("insert into UsersTB values(@UserName,@UserSSN,@UserPhone,@UserPass,@UserType)", con);
+            cmd.Parameters.AddWithValue("@UserName", UserName.Text.Trim());
+            cmd.Parameters.AddWithValue("@UserSSN", UserSSN.Text.Trim());
+            cmd.Parameters.AddWithValue("@UserPhone", UserMob.Text.Trim());
+            cmd.Parameters.AddWithValue("@UserPass", UserPassword.Text.Trim());
+            cmd.Parameters.AddWithValue("@UserType", AdminAddUserDropDownList.SelectedValue.ToString());
+            cmd.ExecuteNonQuery();
+
+            Response.Write("<script>alert('تم اضافة المستخدم')</script>");
+
+            ClearForm();
+        }
+
+        protected void SearchBtn_Click(object sender, EventArgs e)
+        {
+            GetUser();
+        }
+
+        protected void EdirBtn_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("update UsersTB set UserName=@UserName ,UserSSN=@UserSSN ,UserPhone =@UserPhone , UserPass=@UserPass , UserType=@UserType where UserSSN = '" + UserSSN.Text.Trim() + "'", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            cmd.Parameters.AddWithValue("@UserName", UserName.Text.Trim());
+            cmd.Parameters.AddWithValue("@UserSSN", UserSSN.Text.Trim());
+            cmd.Parameters.AddWithValue("@UserPhone", UserMob.Text.Trim());
+            cmd.Parameters.AddWithValue("@UserPass", UserPassword.Text.Trim());
+            cmd.Parameters.AddWithValue("@UserType", AdminAddUserDropDownList.SelectedValue.ToString());
+            cmd.ExecuteNonQuery();
+            Response.Write("<script>alert('تم التعديل')</script>");
+            ClearForm();
+        }
+
+        protected void DeleteBtn_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("Delete from UsersTB where UserSSN = '" + UserSSN.Text.Trim() + "' ", con);
+            cmd.ExecuteNonQuery();
+            Response.Write("<script>alert('تم الحذف')</script>");
+            ClearForm();
+        }
+    }
+}
